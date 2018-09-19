21c12cd33e0840df6e028b32cc98d8148de78b9a
Pierre <haro@netc.fr>
2018-09-19 15:55:28 +0200

    commit meta

--- Assets/Script/Pierre/Test.cs
+++ Assets/Script/Pierre/Test.cs
@@ -13,8 +13,8 @@ public class Test : MonoBehaviour {
 	void Update () {
         float ySpeed = rb.velocity.y;
         float xSpeed = rb.velocity.x;
-        if (Input.GetKey(KeyCode.LeftArrow)) rb.velocity = new Vector2(xSpeed+0.3f, 0);
-        if (Input.GetKey(KeyCode.RightArrow)) rb.velocity = new Vector2(xSpeed-0.3f,0);
+        if (Input.GetKey(KeyCode.LeftArrow)) rb.velocity = new Vector2(xSpeed-0.3f, 0);
+        if (Input.GetKey(KeyCode.RightArrow)) rb.velocity = new Vector2(xSpeed+0.3f,0);
         if (Input.GetKeyUp(KeyCode.LeftArrow) && rb.velocity == new Vector2( 0, 0)) rb.velocity = new Vector2(xSpeed - 0.1f, 0);
     }
 }
