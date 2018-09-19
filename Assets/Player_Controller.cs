a183879a34b58a2c32b295222ac5fe2dfd164353
JeanBon51 <julien.barande@gmail.com>
2018-09-19 16:47:04 +0200

    deplacement

---
+++ Assets/Player_Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    public float Acceleration;
+    public float MaxVitesse;
+    public float Friction;
+
+    private Rigidbody2D Rb;
+
+    // Use this for initialization
+    void Start () {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 inputValue = new Vector2(-Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if(inputValue != Vector2.zero)
+        {
+            Rb.velocity += inputValue.normalized * Acceleration * Time.deltaTime;
+            if(Rb.velocity.magnitude > MaxVitesse)
+            {
+                Rb.velocity = Rb.velocity.normalized * MaxVitesse;
+            }
+        }
+        else if (Rb.velocity != Vector2.zero)
+        {
+            if (Rb.velocity.magnitude > Friction * Time.deltaTime)
+                Rb.velocity -= Rb.velocity.normalized * Friction * Time.deltaTime;
+            else
+            {
+                Rb.velocity = Vector2.zero;
+            }
+        }
+
+	}
+}
