413f7bc790b71d00a62ca65fe82ed7c7531df92a
TITATRIX\TitatriX <dorian.gelas@gmail.com>
2018-09-19 15:38:58 +0200

    Je suis un abruti, désolé :(

--- Assets/Script/Dorian/BringUpPauseMenu.cs
+++ Assets/Script/Dorian/BringUpPauseMenu.cs
@@ -9,7 +9,24 @@ public class BringUpPauseMenu : MonoBehaviour {
 
     void Update()
     {
+        ///Si le Joueur appuie sur échap, le jeu passe en état de pause et affiche le menu pause, si il appuie sur échap à nouveau, le menu pause disparait et le jeu reprend
         if (Input.GetKey("escape"))
+        {
+            if (StatePause == false)
+            {
+                StatePause = true;
+                Time.timeScale = 0;
+                MenudePause.SetActive(true);
+            }
+            if (StatePause == true)
+            {
+                StatePause = false;
+                Time.timeScale = 1;
+                MenudePause.SetActive(true);
+            }
+        }
+        ///Si le Joueur appuie sur le boutton Start, le jeu passe en état de pause et affiche le menu pause, si il appuie sur le boutton Start à nouveau, le menu pause disparait et le jeu reprend
+        if (Input.GetButton("Start"))
         {
             if (StatePause == false)
             {
--- Assets/Script/Dorian/DeathScript.cs
+++ Assets/Script/Dorian/DeathScript.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class DeathScript : MonoBehaviour {
 
-    public GameObject Gameobjects;
+    public GameObject[] Gameobjects;
+    public GameObject MenuDeFin;
+    public bool StateOver;
 
     void OnTriggerEnter(Collider other)
     {
-        Gameobjects = GameObject.FindGameObjectsWithTag("Respawn");
+        Collider.GetComponement<GameObject>
     }
 
+    void FixedUpdate()
+    {
+        Gameobjects = GameObject.FindGameObjectsWithTag("Player");
+        if (Gameobjects.Length == 0)
+        {
+            StateOver = true;
+            MenuDeFin.SetActive(true);
+        }
+    }
     
-    if (gameObjects.Length == 0)
 }
