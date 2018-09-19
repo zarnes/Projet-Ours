0d425f4f67190a530def95dec1d9a507e06f144d
Pierre <haro@netc.fr>
2018-09-19 14:12:25 +0200

    prefab player
    Add prefab player

---
+++ Assets/Script/Pierre/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    bool verticalTranslation_active;
+    bool horizontalTranslation_active;
+    float speed = 0.2f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical_translation = Input.GetAxis("Vertical") * speed;
+        float horizontal_translation = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(horizontal_translation, 0, 0);
+        transform.Translate(0, vertical_translation, 0);
+    }
+}
---
+++ Assets/Script/Pierre/Multiplication.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiplication : MonoBehaviour {
+    int numberOfCells;
+    GameObject cellule;
+    SpriteRenderer spriteRenderer;
+    PolygonCollider2D celluleCollider;
+    // Use this for initialization
+    void Start () {
+        numberOfCells = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.M) && numberOfCells<4) 
+        {
+            Debug.Log("MULTIPLICATION");
+            multiplication();
+        }
+	}
+
+    
+
+    void multiplication ()
+    {
+        numberOfCells += 1;
+        cellule = GameObject.Find("cellule" + numberOfCells);
+        if (cellule == null) Debug.Log("Objet non trouvé");
+        spriteRenderer = cellule.GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = true;
+        celluleCollider = cellule.GetComponent<PolygonCollider2D>();
+        celluleCollider.enabled = true;
+
+    }
+}
