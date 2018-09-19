566afa806d37a18a1d2cb9e2edbb931d03ee97cd
Romain Foucher <zarnes@live.com>
2018-09-19 11:55:23 +0200

    Modified structure Finished enemy AI Started currents

---
+++ Assets/Script/Rom/Buoyancy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buoyancy : MonoBehaviour
+{
+    public Vector3 direction;
+    public float MinSpeed;
+    public float MaxSpeed;
+    public float Acceleration;
+
+    private Dictionary<Transform, float> _entities = new Dictionary<Transform, float>();
+    	
+	// Update is called once per frame
+	void Update ()
+    {
+        foreach (KeyValuePair<Transform, float> entity in _entities)
+        {
+            entity.Key.Translate(direction * entity.Value * Time.deltaTime);
+            _entities[entity.Key] += Acceleration * Time.deltaTime;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + " entering");
+        // If already exist
+        if (_entities.ContainsKey(other.transform))
+        {
+            Debug.LogWarning(other.name + " is trying to enter buoyancy zone but is already in.");
+            return;
+        }
+
+        _entities.Add(transform, MinSpeed);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.name + " leaving");
+
+        // If object not found
+        if (_entities.ContainsKey(other.transform))
+        {
+            Debug.LogWarning(other.name + " is trying to leave buoyency zone but is not inside it.");
+            return;
+        }
+
+        _entities.Remove(other.transform);
+    }
+}
---
+++ Assets/Script/Rom/WhiteCellBehaviour.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WhiteCellBehaviour : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private float _nextCalculation;
+
+    public float AggroDistance;
+    public float AbandonDistance;
+
+    public Transform Player;
+    public float RecalculationTime;
+
+    public bool IsFollowingPlayer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        _agent = GetComponent<NavMeshAgent>();
+        _nextCalculation = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float distance = Vector3.Distance(Player.position, transform.position);
+        Vector3 direction = Player.position - transform.position;
+        RaycastHit hit;
+
+        // If close enough, check raycast then follow player
+        if (distance < AggroDistance && !IsFollowingPlayer)
+        {
+            if (Physics.Raycast(transform.position, direction, out hit, distance))
+            {
+                if (hit.transform.tag == "Player")
+                {
+                    IsFollowingPlayer = true;
+                }
+            }
+            
+        }
+        // If too far, abandon player
+        else if (distance > AbandonDistance && IsFollowingPlayer)
+        {
+            Stop();
+        }
+        
+        if (IsFollowingPlayer && Time.time > _nextCalculation)
+        {
+            FollowPlayer();
+
+            // If the ennemy is not visible anymore, abandon player
+            if (distance < AbandonDistance && Physics.Raycast(transform.position, direction, out hit, distance))
+            {
+                if (hit.transform.tag != "Player")
+                    Stop();
+            }
+        }
+	}
+
+    private void Stop()
+    {
+        IsFollowingPlayer = false;
+        _nextCalculation = Time.time;
+        _agent.SetDestination(transform.position);
+    }
+
+    private void FollowPlayer()
+    {
+        _nextCalculation = Time.time + RecalculationTime;
+        _agent.SetDestination(Player.transform.position);
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, AggroDistance);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, AbandonDistance);
+    }
+}
