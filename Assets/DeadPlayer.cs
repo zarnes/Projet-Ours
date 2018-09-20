d8eee45e80dcd455d435fba1fa88b5af28ffb190
JeanBon51 <julien.barande@gmail.com>
2018-09-20 11:13:48 +0200

    level 1

---
+++ Assets/DeadPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadPlayer : MonoBehaviour {
+
+    public GameObject mortSprite;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Enemi")
+        {
+            
+            //Instantiate(mortSprite);
+            SceneManager.LoadScene("level1");
+            Destroy(gameObject);
+
+        }
+        if (collision.gameObject.tag == "DeadZone")
+        {
+
+            //Instantiate(mortSprite);
+            SceneManager.LoadScene("level1");
+            Destroy(gameObject);
+
+        }
+    }
+}
