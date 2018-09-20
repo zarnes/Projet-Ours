70564c4fc7123469f9c52fbb2edcf9d9b9cfcbec
Pierre <haro@netc.fr>
2018-09-20 08:10:41 +0200

    coomit

--- Assets/Script/Pierre/Multiplication.cs
+++ Assets/Script/Pierre/Multiplication.cs
@@ -3,35 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Multiplication : MonoBehaviour {
-    int numberOfCells;
-    GameObject cellule;
-    SpriteRenderer spriteRenderer;
-    PolygonCollider2D celluleCollider;
-    // Use this for initialization
+
+    int numberOfPlayer;
+    GameObject player2WithSprite;
+    PolygonCollider2D pc;
+    SpriteRenderer sr;
+    GameObject player1;
+    GameObject playerClone;
+
     void Start () {
-        numberOfCells = 1;
-	}
+        numberOfPlayer = 1;
+        player2WithSprite = GameObject.Find("player1_cellule2");
+        player1 = GameObject.Find("player1");
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyUp(KeyCode.M) && numberOfCells<4) 
+        //Active le deuxième sprite du GameObject player1
+        if (Input.GetKeyUp(KeyCode.M) && numberOfPlayer<2) 
         {
-            Debug.Log("MULTIPLICATION");
             multiplication();
+            numberOfPlayer = 2;
         }
-	}
+
+        if (Input.GetKeyUp(KeyCode.B)&&GameObject.Find("player1(Clone)")==null&&numberOfPlayer==2)
+        {
+            pc = player2WithSprite.GetComponent<PolygonCollider2D>();
+            pc.enabled = false;
+            sr = player2WithSprite.GetComponent<SpriteRenderer>();
+            sr.enabled = false;
+            playerClone = Instantiate(player1, player2WithSprite.transform.position, player2WithSprite.transform.rotation);
+        }
+        else if(Input.GetKeyUp(KeyCode.B) && GameObject.Find("player1(Clone)") != null)
+        {
+           
+            float distance = Vector3.Distance(playerClone.transform.position, player1.transform.position);
+            Debug.Log(distance);
+            if (distance < 4)
+            {
+                Destroy(playerClone);
+                multiplication();
+                numberOfPlayer = 2;
+            }
+            else numberOfPlayer = 1;
+
+
+
+        }
+
+    }
 
     
 
     void multiplication ()
     {
-        numberOfCells += 1;
-        cellule = GameObject.Find("cellule" + numberOfCells);
-        if (cellule == null) Debug.Log("Objet non trouvé");
-        spriteRenderer = cellule.GetComponent<SpriteRenderer>();
-        spriteRenderer.enabled = true;
-        celluleCollider = cellule.GetComponent<PolygonCollider2D>();
-        celluleCollider.enabled = true;
-
+        numberOfPlayer += 1;
+        pc = player2WithSprite.GetComponent<PolygonCollider2D>();
+        sr = player2WithSprite.GetComponent<SpriteRenderer>();
+        sr.enabled=true;
+        pc.enabled = true;
     }
 }
