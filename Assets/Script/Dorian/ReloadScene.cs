f86522bb651a81b17648e547a5004857d9ebcbc7
TITATRIX\TitatriX <dorian.gelas@gmail.com>
2018-09-20 11:20:20 +0200

    Script Hard Reload

---
+++ Assets/Script/Dorian/ReloadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ReloadScene : MonoBehaviour
+{
+    private Scene SceneName;
+    private string SceneNameS;
+
+    public void reloadScene()
+    {
+        SceneName = SceneManager.GetActiveScene();
+        SceneNameS = SceneName.ToString();
+        SceneManager.LoadScene(SceneNameS);
+
+    }
+}
