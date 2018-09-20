7b02c2ebd0a02e3b9349d3af204e85d69b7c397a
TITATRIX\TitatriX <dorian.gelas@gmail.com>
2018-09-20 09:16:59 +0200

    svpé marche le script

--- Assets/Script/Dorian/DeathScript.cs
+++ Assets/Script/Dorian/DeathScript.cs
@@ -2,33 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
 public class DeathScript : MonoBehaviour
 {
-/*
-
     public GameObject[] Gameobjects;
     public GameObject MenuDeFin;
     public bool StateOver;
-
-
-    void OnTriggerEnter(Collider other)
-    {
-        Collider.GetComponement<GameObject>;
-    }
-
+    private GameObject Target;
 
     /// Si l'objet qui touche le collider a le tag player, il se fait détruire
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
-            Destroy(other.GetComponement<GameObject>);
+        if (other.tag == "Player") {
+            Destroy(other.gameObject);
+        }
     }
 
     /// Si la scène ne contient pas d'objet avec un tag player, le jeu passe en état de fin
-
     void FixedUpdate()
     {
         Gameobjects = GameObject.FindGameObjectsWithTag("Player");
@@ -37,7 +26,7 @@ public class DeathScript : MonoBehaviour
             StateOver = true;
             MenuDeFin.SetActive(true);
         }
-    }*/
+    }
 
     
 }
